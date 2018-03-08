0531daaa5bfa393afbdc1b85ac2fe7a4ebfa8b9d
Prakash Chandra Awal <chandiprakash16@gmail.com>
2018-03-08 23:38:24 +0545

    update middleware for localhost

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
   if(req.headers['x-forwarded-proto'] === 'http') {
-    next();
+    res.redirect('http://' + req.hostname + req.url);
+   
   }
   else {
-    res.redirect('http://' + req.hostname + req.url);
+    next();
   }
 });
 
