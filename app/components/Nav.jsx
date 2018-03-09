0b2d17a39f062af5da2f4bf6ffc21854acf7ffca
Prakash Chandra Awal <chandiprakash16@gmail.com>
2018-03-09 23:19:10 +0545

    Added style in nav page

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -14,14 +14,50 @@ var Nav = React.createClass({
     }
 }); */
 
-var Nav = (props) => {
-    return (
-        <div>
-        <h2>Nav Component</h2>
-        <IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to='/about' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to='/example' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+var Nav = React.createClass({
+    onSearch: function(e) {
+        e.preventDefault();
+        alert("Not yet Wired up!");
+    },
+    render: function() {
+        return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+            <ul className="menu">
+                <li className="menu-text">React Weather App</li>
+                <li>
+                <IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+                </li>
+                <li>
+                <Link to='/about' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+                </li>
+                <li>
+                <Link to='/example' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+                </li>
+            </ul>
+            </div>
+
+            <div className="top-bar-right">
+            <form onSubmit={this.onSearch}>
+            <ul className="menu">
+                <li><input type="search" placeholder="Search weather"/></li>
+                <li><input type="submit" className="button" value="Get Weather"/></li>
+            </ul>
+            </form>
+            </div>
         </div>
     );
-}
+    }
+});
+  
+
 module.exports = Nav;
+
+var old = (
+    <div>
+        <h2>Nav Component</h2>
+       
+      
+        
+    </div>
+);
