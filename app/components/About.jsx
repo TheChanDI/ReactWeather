c5334d525afe6fccdf5f07114877854e3ade98f4
Prakash Chandra Awal <chandiprakash16@gmail.com>
2018-03-11 21:35:28 +0545

    updated the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,9 +10,27 @@ var About = React.createClass({
 
 var About = (props) => {
     return (
-        <div>
-        <h3>About!</h3>
-        <p>This app is a simple weather app which provides current weather of your city. This is developed from React js.</p>
+        <div> 
+        <h1 className="text-center">About this app</h1>
+        <p>This app is a simple weather app which provides current weather of your city.
+         This is built on React.</p>
+        <p>
+            Here are some of the tools i used:
+        </p>
+        <ul>
+            <li>
+                <a href="https://facebook.github.io/react" target="_blank">React</a> - This was the 
+                JavaScript framework used.
+            </li>
+            <li>
+                <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> - I used
+                Open Weather Map to search for weather data by city name.
+            </li>
+            <li>
+                <a href="https://foundation.zurb.com/" target="_blank">Zurb Foundation</a> - I used this framework
+                for layouting and responsive.
+            </li>
+        </ul>
         </div>
     );
 };
